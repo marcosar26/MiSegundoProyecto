fceff7d635dcb9c4a6aa6e6d247994dc4b844ff0
chiteroman <querveros999@gmail.com>
2024-09-28 22:04:17 +0200

    first commit

---
+++ src/main/java/com/dwes/MiSegundoProyecto/MiSegundoProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.dwes.MiSegundoProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiSegundoProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiSegundoProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwes/MiSegundoProyecto/MiSegundoProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwes.MiSegundoProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiSegundoProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
